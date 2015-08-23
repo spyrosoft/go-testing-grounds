0ce825fbf431bb2c86f127bca229540d45471f1b
Bennett Roesch <meta@spyrosoft.com>
2015-08-22 18:02:28 -0700

    Add second solution to Loops and Functions.
    Also remove the binary which should not have been committed.

--- tour-of-go/loops-and-functions.go
+++ tour-of-go/loops-and-functions.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func Sqrt(x float64) float64 {
 	var z float64 = 4
-	for i := 0; i < 10; i++ {
+	var i int = 0
+	for ; z != math.Sqrt(x); i++ {
 		z = z - ( ((z * z) - x) / (2 * z) )
 	}
+	fmt.Println(i)
 	return z
 }
 
