6a58bb979652869f52b9d5ff012b73ceb3c96fdd
Bennett Roesch <meta@spyrosoft.com>
2015-08-22 18:26:15 -0700

    Add a switch example with no condition.

--- tour-of-go/switch/switch.go
+++ tour-of-go/switch/switch.go
@@ -4,16 +4,36 @@ import (
 	"fmt"
 )
 
-func main() {
+func fallthroughExample() {
 	switch example := "example"; example {
 	case "other":
 		fmt.Println("Other")
 	case "example":
 		fmt.Println("Example")
+		//Notice the fallthrough statement is opposite of C (Yay!!)
 		fallthrough
 	case "twizzler":
 		fmt.Println("Twizzler")
 	default:
 		fmt.Printf("Default")
 	}
+}
+
+func noConditionExample() {
+	currentTime := time.Now()
+	switch {
+	case currentTime.Hour() < 12:
+		//Referring to the Yahoo Answers meme
+		fmt.Println( "Good Mroing." )
+	case currentTime.Hour() < 17:
+		//Futurama
+		fmt.Println( "Good News, Everyone!" )
+	default:
+		fmt.Println( "Good Evening." )
+	}
+}
+
+func main() {
+	fallthroughExample()
+	noConditionExample()
 }
