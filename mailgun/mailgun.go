6080cb17f8e1b41b49be51d2c03cbe5698afc3f7
Bennett Roesch <meta@spyrosoft.com>
2017-02-16 13:34:55 -0800

    Freshen up the mailgun example.

--- mailgun/mailgun.go
+++ mailgun/mailgun.go
@@ -1,53 +1,57 @@
 package main
 
 import (
-	"fmt"
-	"github.com/mailgun/mailgun-go"
-	"os"
-	"io/ioutil"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
+
+	"github.com/mailgun/mailgun-go"
 )
 
 type Config struct {
-	Domain string `json:"domain"`
+	Domain     string `json:"domain"`
+	Sender     string `json:"sender"`
+	Recipient  string `json:"recipient"`
 	PrivateKey string `json:"private-key"`
-	PublicKey string `json:"public-key"`
 }
 
-func initializeConfig() Config {
-	newConfig := Config{}
-	var receivedSettings []byte
-	receivedSettings, error := ioutil.ReadAll( os.Stdin )
-	exitOnError( error )
-	error = json.Unmarshal( receivedSettings, &newConfig )
-	exitOnError( error )
-	return newConfig
+var (
+	config Config
+)
+
+func main() {
+	initializeConfig()
+	sendEmail()
+	fmt.Println("Success!!")
+}
+
+func initializeConfig() {
+	var configJson []byte
+	configJson, err := ioutil.ReadAll(os.Stdin)
+	exitOnErr(err)
+	err = json.Unmarshal(configJson, &config)
+	exitOnErr(err)
 }
 
 func sendEmail() {
-	config := initializeConfig()
-	mailgunInstance := mailgun.NewMailgun( config.Domain, config.PrivateKey, config.PublicKey )
-	
-	messageContentsBytes, error := ioutil.ReadFile( "message.email" )
-	exitOnError( error )
-	
-	messageContents := string( messageContentsBytes )
-	messageContents = strings.Replace( messageContents, "RECIPIENT_NAME", "Batman", 1 )
-	
-	newMessage := mailgunInstance.NewMessage( "example@example.com", "Testing Mailgun", messageContents, "example@example.com" )
-	_, _, error = mailgunInstance.Send( newMessage )
-	exitOnError( error )
+	mg := mailgun.NewMailgun(config.Domain, config.PrivateKey, "")
+
+	messageBytes, err := ioutil.ReadFile("message.email")
+	exitOnErr(err)
+
+	message := string(messageBytes)
+	message = strings.Replace(message, "RECIPIENT_NAME", "Batman", 1)
+
+	mgMessage := mg.NewMessage(config.Sender, "Testing Mailgun", message, config.Recipient)
+	_, _, err = mg.Send(mgMessage)
+	exitOnErr(err)
 }
 
-func exitOnError( error error ) {
-	if error != nil {
-		fmt.Println( error )
-		os.Exit( 1 )
+func exitOnErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
-}
-
-func main() {
-	sendEmail()
-	fmt.Println( "Success!!" )
 }
