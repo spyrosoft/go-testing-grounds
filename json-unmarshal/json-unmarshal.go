1b7ddbedb2ba8acc1adbe81b1c6ad0c7099b2b6f
Bennett Roesch <meta@spyrosoft.com>
2017-04-07 11:27:19 -0700

    Demonstrate string, int, float64, and []string unmarshal

--- json-unmarshal/json-unmarshal.go
+++ json-unmarshal/json-unmarshal.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
-	"encoding/json"
 )
 
-type Message struct {
-	Name string `json:"example-name"`
+type Example struct {
+	String      string   `json:"string"`
+	Int         int      `json:"int"`
+	Float       float64  `json:"float"`
+	StringArray []string `json:"string-array"`
 }
 
 func main() {
-	var unmarshaling_with_tag Message
-	json_input := []byte( `{"example-name":"Eeeeee!"}` )
-	err := json.Unmarshal( json_input, &unmarshaling_with_tag )
+	var e Example
+	input := `
+{
+	"string" : "string"
+	, "int" : 4
+	, "float" : 4.5
+	, "string-array" : ["string-1", "string-2"]
+}
+`
+	err := json.Unmarshal([]byte(input), &e)
 	if err != nil {
-		os.Exit( 1 )
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
-	fmt.Println( unmarshaling_with_tag )
+	fmt.Printf("%+v\n", e)
+	fmt.Println()
 }
